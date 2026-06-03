a3b641cf06ea6ed3eef12af47502df25d414f417
Dillon Johnson <me@nollidnosnhoj.com>
2026-06-02 16:42:34 -1000

    setup dotnet and spire

--- dot_config/fish/conf.d/dotnet.fish
+++ dot_config/fish/conf.d/dotnet.fish
@@ -3,3 +3,11 @@ if test -d $HOME/.dotnet
         set -p PATH $HOME/.dotnet/tools
     end
 end
+
+set -gx ASPIRE_CONTAINER_RUNTIME podman
+
+# Added by get-aspire-cli.sh
+fish_add_path $HOME/.aspire/bin
+
+# for dev certs
+set -gx SSL_CERT_DIR "$HOME/.aspnet/dev-certs/trust:/etc/ssl/certs"
